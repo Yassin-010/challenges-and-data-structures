8df6edcd1dc0f24fb3eba18ec9843195eeecd141
Yassin-010 <yassinse898@gmail.com>
2024-06-28 19:07:53 +0300

    done

---
+++ Challenges/Find-Duplicates/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace challenges_and_data_structures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input1 = { 1, 2, 3, 1, 2, 3 };
+            int[] duplicates1 = FindDuplicates(input1);
+            Console.WriteLine("Input: [1, 2, 3, 1, 2, 3]");
+            Console.WriteLine("Output: [" + string.Join(", ", duplicates1) + "]");
+            Console.WriteLine();
+
+            int[] input2 = { 16, 8, 31, 17, 15, 23, 17, 8 };
+            int[] duplicates2 = FindDuplicates(input2);
+            Console.WriteLine("Input: [16, 8, 31, 17, 15, 23, 17, 8]");
+            Console.WriteLine("Output: [" + string.Join(", ", duplicates2) + "]");
+            Console.WriteLine();
+
+            int[] input3 = { 5, 10, 16, 20, 10, 16 };
+            int[] duplicates3 = FindDuplicates(input3);
+            Console.WriteLine("Input: [5, 10, 16, 20, 10, 16]");
+            Console.WriteLine("Output: [" + string.Join(", ", duplicates3) + "]");
+            Console.WriteLine();
+        }
+
+        public static int[] FindDuplicates(int[] arr)
+        {
+            List<int> duplicates = new List<int>();
+            Dictionary<int, int> countMap = new Dictionary<int, int>();
+
+            foreach(int num in arr)
+            {
+                if(countMap.ContainsKey(num))
+                    countMap[num]++;
+                else
+                    countMap[num] = 1;
+            }
+
+            foreach(KeyValuePair<int, int> entry in countMap)
+            {
+                if(entry.Value > 1)
+                    duplicates.Add(entry.Key);
+            }
+
+            return duplicates.ToArray();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -32,4 +32,9 @@ Write a function called MiddleValue which takes in an array and a value to be ad
 ![Insert Value in Middle of Array](./whiteboard-challenges/nsert-Value-in-Middle-of-Array.png).
 
 
+### Challenge 05: Find-Duplicates.
+### Description:
+Write a function called FindDuplicates that accepts an array as its parameter. Without utilizing any of the built-in methods provided by the language, the function should return an array of duplicate values.
+![Find Duplicates in Array](./whiteboard-challenges/Find_Duplicates_in_Array.png).
+
 
